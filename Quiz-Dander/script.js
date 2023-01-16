44d0f64bf5eeaae29d52de2854fe7a9005f8123f
Dander <89800@glr.nl>
2023-01-16 12:24:21 +0100

    progress java script

--- Quiz-Dander/script.js
+++ Quiz-Dander/script.js
@@ -1,16 +1,45 @@
 var i = 0;
-var txt = 'hier komt een vraag'; /* The text */
+var txt = 'welk jaar is pac man gemaakt'; /* The text */
 var speed = 90; /* The speed/duration of the effect in milliseconds */
 let timeout;
+var quistion =0;
+var awnser = 0;
 
-function typen() {
-  timeout = setTimeout(typeWriter, 3000);
+function vraag1() {
+  timeout = setTimeout(typeWriter, 2000);
 }
-
 function typeWriter() {
   if (i < txt.length) {
     document.getElementById("vraagtext").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
+    
   }
+  
+}
+
+
+
+function next(){
+  quistion++;
+  if (quistion==1){
+    txt="wie heeft pacman gemaakt";
+    awnser = 3;
+  }
+
+  if (quistion==2){
+    txt="hoe is pacman gemaakt";
+  }
+
+  if (quistion==3){
+    txt="is er een pacman serie";
+  }
+
+}
+
+function writeagain(){
+  i=0;
+    document.getElementById("vraagtext").innerHTML ="";
+    typeWriter();
+
 }
