ef1fb77d7f1ce8022e07eefe048b192280ec7153
[Noah van Ree] <noahvanree1201@gmail.com>
2023-01-18 14:27:58 +0100

    Antwoorden gemaakt

---
+++ Quiz-Noah/script.js
@@ -0,0 +1,19 @@
+
+function antwoord(){
+    vraag1 = document.getElementById("vraag1").value;
+    vraag2 = document.getElementById("vraag2").value;
+    vraag3 = document.getElementById("vraag3").value;
+    vraag4 = document.getElementById("vraag4").value;
+    vraag5 = document.getElementById("vraag5").value;
+
+    if (vraag1 == "Watermeloen" || vraag1 == "watermeloen" || vraag2 == "21 april 2010" || vraag2 == "21 April 2010" || vraag3 == "Halfbrick" || vraag3 == "halfbrick" || vraag4 == "Ja" || vraag4 == "ja" || vraag5 == "Ja" || vraag5 == "ja"){
+        alert("Je hebt het goed!");
+    }else{
+        alert("Je hebt een of meerdere fouten!");
+        alert("Wil je de antwoorden zien? Druk dan op 'Zie antwoorden'")
+    }
+}
+
+function zieantwoord(){
+    alert("Vraag 1: Watermeloen, Vraag 2: 21 april 2010, Vraag 3: Halfbrick, Vraag 4: Ja, Vraag 5: Ja");
+}
