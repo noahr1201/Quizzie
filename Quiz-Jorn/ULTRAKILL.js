9114d3d7e8e63557f474c8322c3479aeeeb21d2c
Esperitz <88886@glr.nl>
2023-01-25 15:52:01 +0100

    sd

--- Quiz-Jorn/ULTRAKILL.js
+++ Quiz-Jorn/ULTRAKILL.js
@@ -9,3 +9,11 @@ let vraag8 = document.getElementsByName("vraag8")
 let vraag9 = document.getElementsByName("vraag9")
 let vraag10 = document.getElementsByName("vraag10")
 
+function submit(){
+    var checkRadio = document.querySelector('input[id="hpup"]:checked', 'input[id="knuckleduster"]:checked', 'input[id="cybergrind"]:checked', 'input[id="four"]:checked:checked', 'input[id="4"]:checked:checked', 'input[id="memyselfandi"]:checked', 'input[id="5"]:checked', 'input[id="ultrakill"]:checked', 'input[id="sins"]:checked', 'input[id="16"]:checked')
+    
+    if(checkRadio != null){
+        document.getElementById("results").innerHTML = checkRadio.value + "hi"
+    }
+    
+}
