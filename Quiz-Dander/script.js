e890d28b14e6aca61d0ea903d008e7bc53f06f12
Dander <89800@glr.nl>
2023-01-25 13:14:39 +0100

    ja

--- Quiz-Dander/script.js
+++ Quiz-Dander/script.js
@@ -1,14 +1,15 @@
-var i = 0;
-var txt = ''; /* The text */
-var speed = 90; /* The speed/duration of the effect in milliseconds */
+let i = 0;
+let txt = ''; /* The text */
+let speed = 90;
+let delay = 700; /* The speed/duration of the effect in milliseconds */
 let timeout;
-var quistion =0;
-var awnser = 0;
-var totaal = 0;
-var awnser1 = document.getElementById('knop1');
-var awnser2 = document.getElementById('knop2');
-var awnser3 = document.getElementById('knop3');
-var awnser4 = document.getElementById('knop4');
+let quistion =0;
+let awnser = 0;
+let totaal = 0;
+let awnser1 = document.getElementById('knop1');
+let awnser2 = document.getElementById('knop2');
+let awnser3 = document.getElementById('knop3');
+let awnser4 = document.getElementById('knop4');
 console.log(quistion);
 console.log(awnser);
 
@@ -29,6 +30,12 @@ function vraag1() {
 }
 function typeWriter() {
   if (i < txt.length) {
+    if (i == 40 || i == 80 || i==120) {
+      document.getElementById("vraagtext").innerHTML += ',<br/>';
+    }
+    let audio = new Audio('../media/media-Dander/gameboy-pluck-41265.mp3');
+audio.play();
+
     document.getElementById("vraagtext").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
@@ -38,7 +45,7 @@ function typeWriter() {
 }
 
 function writeagain2() {
-  timeout = setTimeout(writeagain, 1000);
+  timeout = setTimeout(writeagain, delay);
 }
 
 function writeagain(){
@@ -87,12 +94,57 @@ function next(){
 
   if (quistion==4){
     txt="de quiz is klaar";
+    document.getElementById("nummer1").innerHTML = "info"; 
+    document.getElementById("nummer2").innerHTML = "info";
+    document.getElementById("nummer3").innerHTML = "info";
+    document.getElementById("nummer4").innerHTML = "info";
+  }
+
+  if (quistion==5){
+    document.getElementById("vraagtext").style.fontSize = "3vw";
+    txt="nu kan je lezen over de guiz en de maker";
+    document.getElementById("nummer1").innerHTML = "next"; 
+    document.getElementById("nummer2").innerHTML = "next";
+    document.getElementById("nummer3").innerHTML = "next";
+    document.getElementById("nummer4").innerHTML = "next";
+  }
+
+  if (quistion==6){
+    document.getElementById("vraagtext").style.fontSize = "3vw";
+    txt="deze quiz is gemaakt door Dander Siegers uit klas D1B";
+    document.getElementById("nummer1").innerHTML = "next"; 
+    document.getElementById("nummer2").innerHTML = "next";
+    document.getElementById("nummer3").innerHTML = "next";
+    document.getElementById("nummer4").innerHTML = "next";
+  }
+
+  if (quistion==7){
+    document.getElementById("vraagtext").style.fontSize = "3vw";
+    txt="de quiz ging over pacman. pacman is een oude game uit de jaren 80. In het spel moet je allen snoepjes eten terwijl je de spokjes moet ontwijken. ";
+    document.getElementById("nummer1").innerHTML = "next"; 
+    document.getElementById("nummer2").innerHTML = "next";
+    document.getElementById("nummer3").innerHTML = "next";
+    document.getElementById("nummer4").innerHTML = "next";
+  }
+
+  if (quistion==8){
+    document.getElementById("textheader").innerHTML = "home"
+    txt="klaar";
     document.getElementById("nummer1").innerHTML = "..."; 
     document.getElementById("nummer2").innerHTML = "...";
     document.getElementById("nummer3").innerHTML = "...";
     document.getElementById("nummer4").innerHTML = "...";
+    textnormaal2();
   }
 
+}
+
+function textnormaal2() {
+  timeout = setTimeout(textnormaal, delay);
+}
+
+function textnormaal(){
+  document.getElementById("vraagtext").style.fontSize = "4vw";
 }
 
 
@@ -115,7 +167,7 @@ function correct1(){
     totaal++;
     console.log("correct");
     document.getElementById("textheader").innerHTML = "je hebt "+totaal+" punten"
-  }else if (quistion>=4){
+  }else if (quistion==4){
     document.getElementById("textheader").innerHTML = "je hebt "+totaal+" punten"
   }else{
     console.log("incorrect");
