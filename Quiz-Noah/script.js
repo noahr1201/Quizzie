4e4f856866a9ea6be84abe76ba650adbd600d738
[Noah van Ree] <noahvanree1201@gmail.com>
2023-01-25 15:31:17 +0100

    Enabled punten in header

--- Quiz-Noah/script.js
+++ Quiz-Noah/script.js
@@ -11,13 +11,70 @@ function antwoord(){
     vraag9 = document.getElementById("vraag9").value;
     vraag10 = document.getElementById("vraag10").value;
     naam = document.getElementById("name").value;
+    punten = 0;
 
-    if (vraag1 == "Watermeloen" || vraag1 == "watermeloen" || vraag2 == "21 april 2010" || vraag2 == "21 April 2010" || vraag3 == "Halfbrick" || vraag3 == "halfbrick" || vraag4 == "Ja" || vraag4 == "ja" || vraag5 == "Ja" || vraag5 == "ja" || vraag6 == "15" || vraag6 == "vijftien" || vraag7 == "Ja" || vraag7 == "ja" || vraag8 == "Ja" || vraag8 == "ja" || vraag9 == "Arcade" || vraag9 == "arcade" || vraag10 == "200.000" || vraag10 == "200000"){
-        alert("Je hebt het goed!" + " " + naam);
+    if (vraag1 == "Watermeloen" || vraag1 == "watermeloen"){
+        alert("Je hebt vraag 1 goed!" + " " + naam + "" + " " + "Je hebt 1 punt verdiend!");
+        punten ++;
     }else{
-        alert("Je hebt een of meerdere fouten!" + " " + naam);
-        alert("Wil je de antwoorden zien? Druk dan op 'Zie antwoorden'" + " " + naam);
+        alert("Vraag 1 is fout!" + " " + naam)
     }
+    if (vraag2 == "21 april 2010" || vraag2 == "21 April 2010"){
+        alert("Je hebt vraag 2 goed!" + " " + naam + "" + " " + "Je hebt 1 punt verdiend!");
+        punten ++;
+    }else{
+        alert("Vraag 2 is fout!" + " " + naam)
+    }
+    if (vraag3 == "Halfbrick" || vraag3 == "halfbrick"){
+        alert("Je hebt vraag 3 goed!" + " " + naam + "" + " " + "Je hebt 1 punt verdiend!");
+        punten ++;
+    }else{
+        alert("Vraag 3 is fout!" + " " + naam)
+    }
+    if (vraag4 == "Ja" || vraag4 == "ja"){
+        alert("Je hebt vraag 4 goed!" + " " + naam + "" + " " + "Je hebt 1 punt verdiend!");
+        punten ++;
+    }else{
+        alert("Vraag 4 is fout!" + " " + naam)
+    }
+    if (vraag5 == "Ja" || vraag5 == "ja"){
+        alert("Je hebt vraag 5 goed!" + " " + naam + "" + " " + "Je hebt 1 punt verdiend!");
+        punten ++;
+    } else{
+        alert("Vraag 5 is fout!" + " " + naam)
+    }
+    if (vraag6 == "15" || vraag6 == "vijftien"){
+        alert("Je hebt vraag 6 goed!" + " " + naam + "" + " " + "Je hebt 1 punt verdiend!");
+        punten ++;
+    } else{
+        alert("Vraag 6 is fout!" + " " + naam)
+    }
+    if (vraag7 == "Ja" || vraag7 == "ja"){
+        alert("Je hebt vraag 7 goed!" + " " + naam + "" + " " + "Je hebt 1 punt verdiend!");
+        punten ++;
+    } else{
+        alert("Vraag 7 is fout!" + " " + naam)
+    }
+    if (vraag8 == "Ja" || vraag8 == "ja"){
+        alert("Je hebt vraag 8 goed!" + " " + naam + "" + " " + "Je hebt 1 punt verdiend!");
+        punten ++;
+    } else{
+        alert("Vraag 8 is fout!" + " " + naam)
+    }
+    if (vraag9 == "Arcade" || vraag9 == "arcade"){
+        alert("Je hebt vraag 9 goed!" + " " + naam + "" + " " + "Je hebt 1 punt verdiend!");
+        punten ++;
+    } else{
+        alert("Vraag 9 is fout!" + " " + naam)
+    }
+    if (vraag10 == "200.000" || vraag10 == "200000"){
+        alert("Je hebt vraag 10 goed!" + " " + naam + "" + " " + "Je hebt 1 punt verdiend!");
+        punten ++;
+    }else{
+        alert("Vraag 10 is fout!" + " " + naam)
+    }
+    alert("Je hebt" + " " + punten + " " + "punten behaald!" + " " + naam);
+    document.getElementById("punten").innerHTML = punten + " " + "Punten";
 }
 
 function zieantwoord(){
