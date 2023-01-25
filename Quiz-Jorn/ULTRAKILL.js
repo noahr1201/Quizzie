f76136f6fe8ed07e98015db3c68a07ec7973f5a6
Esperitz <88886@glr.nl>
2023-01-25 16:11:43 +0100

    my 13th reason why

--- Quiz-Jorn/ULTRAKILL.js
+++ Quiz-Jorn/ULTRAKILL.js
@@ -8,12 +8,21 @@ let vraag7 = document.getElementsByName("vraag7")
 let vraag8 = document.getElementsByName("vraag8")
 let vraag9 = document.getElementsByName("vraag9")
 let vraag10 = document.getElementsByName("vraag10")
+let answer1 = document.getElementById("ultra")
+let answer2 = document.getElementById("knuckleduster")
+let answer3 = document.getElementById("cyber")
+let answer4 = document.getElementById("four")
+let answer5 = document.getElementById("three")
+let answer6 = document.getElementById("sword")
+let answer7 = document.getElementById("5")
+let answer8 = document.getElementById("ultrakill")
+let answer9 = document.getElementById("sins")
+let answer10 = document.getElementById("sixteen")
+let punten = 0;
 
-function submit(){
-    var checkRadio = document.querySelector('input[id="hpup"]:checked', 'input[id="knuckleduster"]:checked', 'input[id="cybergrind"]:checked', 'input[id="four"]:checked:checked', 'input[id="4"]:checked:checked', 'input[id="memyselfandi"]:checked', 'input[id="5"]:checked', 'input[id="ultrakill"]:checked', 'input[id="sins"]:checked', 'input[id="16"]:checked')
-    
-    if(checkRadio != null){
-        document.getElementById("results").innerHTML = checkRadio.value + "hi"
-    }
-    
+if(vraag1 == answer1){
+    punten++;
+}
+function finish(){
+    console.log("punten")
 }
