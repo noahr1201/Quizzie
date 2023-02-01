54cf46039ee4ad33604bc7f1e00d461ce8013c08
Esperitz <88886@glr.nl>
2023-02-01 13:41:14 +0100

    Quiz werkt (eindelijk)

--- Quiz-Jorn/ULTRAKILL.js
+++ Quiz-Jorn/ULTRAKILL.js
@@ -1,28 +1,19 @@
-let vraag1 = document.getElementsByName("vraag1")
-let vraag2 = document.getElementsByName("vraag2")
-let vraag3 = document.getElementsByName("vraag3")
-let vraag4 = document.getElementsByName("vraag4")
-let vraag5 = document.getElementsByName("vraag5")
-let vraag6 = document.getElementsByName("vraag6")
-let vraag7 = document.getElementsByName("vraag7")
-let vraag8 = document.getElementsByName("vraag8")
-let vraag9 = document.getElementsByName("vraag9")
-let vraag10 = document.getElementsByName("vraag10")
-let answer1 = document.getElementById("ultra")
-let answer2 = document.getElementById("knuckleduster")
-let answer3 = document.getElementById("cyber")
-let answer4 = document.getElementById("four")
-let answer5 = document.getElementById("three")
-let answer6 = document.getElementById("sword")
-let answer7 = document.getElementById("5")
-let answer8 = document.getElementById("ultrakill")
-let answer9 = document.getElementById("sins")
-let answer10 = document.getElementById("sixteen")
-let punten = 0;
+function getResults() {
 
-if(vraag1 == answer1){
-    punten++;
-}
-function finish(){
-    console.log("punten")
-}
+    //CORRECT ANSWERS
+    var amountCorrect = 0;
+  
+    for(var i = 1; i <= 10; i++) {
+      var radiosName = document.getElementsByName('vraag'+i);
+  
+      for(var j = 0; j < radiosName.length; j++) {
+        var radiosValue = radiosName[j];
+        if(radiosValue.value == "juist" && radiosValue.checked) {
+          amountCorrect++;
+        }
+      }
+    }
+  document.getElementById('results').innerHTML =
+    "Aantal correct: " + amountCorrect;
+  
+  }
